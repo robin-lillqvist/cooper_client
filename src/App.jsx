3297234a2913d7c8790b21724d847f9d545c0a50
Robin Lillqvist <robin.lillqvist@gmail.com>
2020-02-26 13:54:08 +0100

    added user authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import DisplayCooperResult from "./components/DisplayCooperResult";
 import InputFields from './components/InputFields';
-
+import LoginForm from './components/LoginForm';
+import { authenticate } from './modules/auth';
 import './App.css';
 
 
@@ -9,17 +10,58 @@ class App extends Component {
   state = {
     distance: "",
     gender: "female",
-    age: ""
+    age: "",
+    renderLoginForm: false ,
+    authenticated: false,
+    message: ""
   };
 
   onChangeHandler = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
+  onLogin = async e => {
+    e.preventDefault();
+    const response = await authenticate(
+      e.target.email.value,
+      e.target.password.value
+    );
+    if (response.authenticated) {
+      this.setState({ authenticated: true });
+    } else {
+      this.setState({ message: response.message, renderLoginForm: false });
+    }
+  };
+
   render() {
+    const { renderLoginForm, authenticated, message } = this.state;
+    let renderLogin;
+    switch(true) {
+      case renderLoginForm && !authenticated:
+        renderLogin = <LoginForm submitFormHandler={this.onLogin} />;
+        break;
+      case !renderLoginForm && !authenticated:
+        renderLogin = (
+          <>
+            <button
+              id="login"
+              onClick={() => this.setState({ renderLoginForm: true })}
+            >Login</button>
+            <p>{message}</p>
+          </>
+        );
+        break;
+      case authenticated:
+        renderLogin = (
+          <p>Hi {JSON.parse(sessionStorage.getItem("credentials")).uid}</p>
+        );
+        break;
+    }
+
     return (
       <>
-        <InputFields onChangeHandler={this.onChangeHandler} />
+      <InputFields onChangeHandler={this.onChangeHandler} />
+        {renderLogin}
         <DisplayCooperResult
           distance={this.state.distance}
           gender={this.state.gender}
