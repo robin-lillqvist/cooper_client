3bce9a60d9809c518bcb773491ebb9ff248b50d8
Carlos <carlos.delgado@hotmail.it>
2020-02-28 10:46:08 +0100

    adding url to connect netlify to heroku

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3000/api/v1";
+axios.defaults.baseURL = "https://cooper-challenge.herokuapp.com/";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
